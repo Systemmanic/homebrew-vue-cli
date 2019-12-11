0d51c51d04c76dd35af485dda3bece76080c8d6e
David Kane <dkanejs@gmail.com>
2019-12-10 23:29:33 -0800

    Update version to 4.1.1
    remove redundant version declaration as version is scanned from URL

--- Formula/vue-cli.rb
+++ Formula/vue-cli.rb
@@ -3,8 +3,7 @@ require "language/node"
 class VueCli < Formula
   desc "Standard Tooling for Vue.js Development"
   homepage "https://cli.vuejs.org/"
-  url "https://registry.npmjs.org/@vue/cli/-/cli-3.11.0.tgz"
-  version "3.11.0"
+  url "https://registry.npmjs.org/@vue/cli/-/cli-4.1.1.tgz"
   sha256 "a38b37599a8601e967ea3fc2de7e3491bb89f5864e3add6229f25a4fad115928"
 
   depends_on "node"
